3917a6cb2d78f75c8d65bffb6e0654aad666b6e1
Batchen <b0504144053@gmail.com>
2024-03-28 13:18:08 +0200

    checking function

--- ex_1/batchen_server/routers/client.js
+++ ex_1/batchen_server/routers/client.js
@@ -2,6 +2,20 @@ var express = require("express");
 var router = express.Router();
 var client = require("../services/client")
 
+const checking = (body) => {
+    const regexForID = /^\d{9}$/;
+    const regexForPhone = /^\d{9,10}$/;
+    if (Object.keys(body).length === 0)
+        return false
+    if (!(regexForID.test(body.idClient) && body.idClient.length === 9))
+        return false
+    if (body.clientFirstName == "" || body.clientLastName == "" || body.address == "" || body.cityName == "" || body.telephone == "" || body.mobilePhone == "")
+        return false
+    if (body.telephone.length < 9 || body.telephone.length > 10 || !regexForPhone.test(body.telephone) || body.mobilePhone.length < 9 || body.mobilePhone.length > 10 || !regexForPhone.test(body.mobilePhone))
+        return false
+    return true
+}
+
 router.get("/allClients", async(req, res, next) => {
     try {
         let result = await client.allClients()
@@ -12,13 +26,16 @@ router.get("/allClients", async(req, res, next) => {
 });
 
 router.post('/add', async function(req, res, next) {
-    try {
-        let result = await client.createClient(req.body.idClient, req.body.clientFirstName, req.body.clientLastName, req.body.cityName, req.body.address, req.body.dateBirth, req.body.telephone, req.body.mobilePhone);
-        res.json(result);
-    } catch (err) {
-        console.error(`Error`, err.message);
-        next(err);
-    }
+    if (checking(req.body))
+        try {
+            let result = await client.createClient(req.body.idClient, req.body.clientFirstName, req.body.clientLastName, req.body.cityName, req.body.address, req.body.dateBirth, req.body.telephone, req.body.mobilePhone);
+            res.json(result);
+        } catch (err) {
+            console.error(`Error`, err.message);
+            next(err);
+        }
+    else
+        res.json("Error - details of client incorrect");
 });
 
 
@@ -33,13 +50,16 @@ router.delete('/deleteClient', async function(req, res, next) {
 });
 
 router.put('/update', async function(req, res, next) {
-    try {
-        let result = await client.updateClient(req.body.idClient, req.body.clientFirstName, req.body.clientLastName, req.body.cityName, req.body.address, req.body.dateBirth, req.body.telephone, req.body.mobilePhone);
-        res.json(result);
-    } catch (err) {
-        console.error(`Error`, err.message);
-        next(err);
-    }
+    if (checking(req.body))
+        try {
+            let result = await client.updateClient(req.body.idClient, req.body.clientFirstName, req.body.clientLastName, req.body.cityName, req.body.address, req.body.dateBirth, req.body.telephone, req.body.mobilePhone);
+            res.json(result);
+        } catch (err) {
+            console.error(`Error`, err.message);
+            next(err);
+        }
+    else
+        res.json("Error - details of client incorrect");
 });
 
 
--- ex_1/batchen_server/routers/covid19.js
+++ ex_1/batchen_server/routers/covid19.js
@@ -2,15 +2,23 @@ var express = require("express");
 var router = express.Router();
 var covid19 = require("../services/covid19")
 
+const checking = (body) => {
+    if (body.dateStart == "" || body.dateEnd == "")
+        return false
+    return true
+}
 
 router.post('/add', async function(req, res, next) {
-    try {
-        let result = await covid19.createCovid19(req.body.idClient, req.body.dateStart, req.body.dateEnd);
-        res.json(result);
-    } catch (err) {
-        console.error(`Error`, err.message);
-        next(err);
-    }
+    if (checking(req.body))
+        try {
+            let result = await covid19.createCovid19(req.body.idClient, req.body.dateStart, req.body.dateEnd);
+            res.json(result);
+        } catch (err) {
+            console.error(`Error`, err.message);
+            next(err);
+        }
+    else
+        res.json("Error - details of covid-19 incorrect");
 });
 
 router.get("/:id", async(req, res, next) => {
@@ -28,7 +36,10 @@ router.put('/update', async function(req, res, next) {
         let covid19User = await covid19.getCovid19ByID(req.body.idClient)
         let result;
         if (covid19User.length == 0)
-            result = await covid19.createCovid19(req.body.idClient, req.body.dateStart, req.body.dateEnd)
+            if (checking(req.body))
+                result = await covid19.createCovid19(req.body.idClient, req.body.dateStart, req.body.dateEnd)
+            else
+                result = "Error - details of covid-19 incorrect";
         else {
             result = await covid19.updateCovid(req.body.idClient, req.body.dateStart, req.body.dateEnd);
         }
